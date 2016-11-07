155bc3f66d5213368bec4301799a8130c61ca058
Wayne.You <lokiwuxi@gmail.com>
2016-11-07 14:00:45 +0800

    project init

---
+++ app/src/main/java/io/github/uv_lab/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,41 @@
+package io.github.uv_lab.photogallery;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by youwei on 2016/11/7.
+ */
+
+public class PhotoGalleryFragment extends Fragment {
+
+    private RecyclerView mPhotoRecyclerView;
+
+    public static PhotoGalleryFragment newInstance() {
+        return new PhotoGalleryFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+
+        mPhotoRecyclerView = (RecyclerView) v
+                .findViewById(R.id.fragment_photo_gallery_recycler_view);
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+
+        return v;
+    }
+}
