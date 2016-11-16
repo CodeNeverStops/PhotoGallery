a2f9418a7d4a0256481cd2741dbcb0b99a0edf53
Wayne.You <lokiwuxi@gmail.com>
2016-11-16 17:24:48 +0800

    use browser to render image url

--- app/src/main/java/io/github/uv_lab/photogallery/GalleryItem.java
+++ app/src/main/java/io/github/uv_lab/photogallery/GalleryItem.java
@@ -1,5 +1,7 @@
 package io.github.uv_lab.photogallery;
 
+import android.net.Uri;
+
 /**
  * Created by youwei on 2016/11/8.
  */
@@ -8,6 +10,7 @@ public class GalleryItem {
     private String mCaption;
     private String mId;
     private String mUrl;
+    private String mOwner;
 
     public String getCaption() {
         return mCaption;
@@ -31,6 +34,22 @@ public class GalleryItem {
 
     public void setUrl(String url) {
         mUrl = url;
+    }
+
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public void setOwner(String owner) {
+        mOwner = owner;
+    }
+
+    public Uri getPhotoPageUri() {
+        return Uri.parse("http://www.flickr.com/photos/")
+                .buildUpon()
+                .appendPath(mOwner)
+                .appendPath(mId)
+                .build();
     }
 
     @Override
