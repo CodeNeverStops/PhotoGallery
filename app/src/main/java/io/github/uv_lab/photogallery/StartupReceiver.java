a539178c3cdc4e08d5c52fbfcaaca2b10ccdba11
Wayne.You <lokiwuxi@gmail.com>
2016-11-16 16:09:39 +0800

    add broadcast receiver

---
+++ app/src/main/java/io/github/uv_lab/photogallery/StartupReceiver.java
@@ -0,0 +1,23 @@
+package io.github.uv_lab.photogallery;
+
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by youwei on 2016/11/16.
+ */
+
+public class StartupReceiver extends BroadcastReceiver {
+    private static final String TAG = "StartupReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Received broadcast intent: " + intent.getAction());
+
+        boolean isOn = QueryPreferences.isAlarmOn(context);
+        PollService.setServiceAlarm(context, isOn);
+    }
+}
