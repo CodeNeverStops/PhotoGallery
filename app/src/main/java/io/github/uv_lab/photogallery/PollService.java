d62176c512af9b40e2d4e9a25aed984bf5a05872
Wayne.You <lokiwuxi@gmail.com>
2016-11-16 17:02:22 +0800

    send ordered broadcast

--- app/src/main/java/io/github/uv_lab/photogallery/PollService.java
+++ app/src/main/java/io/github/uv_lab/photogallery/PollService.java
@@ -1,5 +1,6 @@
 package io.github.uv_lab.photogallery;
 
+import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.DownloadManager;
 import android.app.IntentService;
@@ -29,6 +30,8 @@ public class PollService extends IntentService {
             "io.github.uv_lab.photogallery.SHOW_NOTIFICATION";
     public static final String PERM_PRIVATE =
             "io.github.uv_lab.photogallery.PRIVATE";
+    public static final String REQUEST_CODE = "REQUEST_CODE";
+    public static final String NOTIFICATION = "NOTIFICATION";
 
     public static Intent newIntent(Context context) {
         return new Intent(context, PollService.class);
@@ -101,14 +104,24 @@ public class PollService extends IntentService {
                     .setAutoCancel(true)
                     .build();
 
-            NotificationManagerCompat notificationManager =
-                    NotificationManagerCompat.from(this);
-            notificationManager.notify(0, notification);
+//            NotificationManagerCompat notificationManager =
+//                    NotificationManagerCompat.from(this);
+//            notificationManager.notify(0, notification);
+//
+//            sendBroadcast(new Intent(ACTION_SHOW_NOTIFICATION), PERM_PRIVATE);
 
-            sendBroadcast(new Intent(ACTION_SHOW_NOTIFICATION), PERM_PRIVATE);
+            showBackgroundNotification(0, notification);
         }
 
         QueryPreferences.setLastResultId(this, resultId);
+    }
+
+    private void showBackgroundNotification(int requestCode, Notification notification) {
+        Intent i = new Intent(ACTION_SHOW_NOTIFICATION);
+        i.putExtra(REQUEST_CODE, requestCode);
+        i.putExtra(NOTIFICATION, notification);
+        sendOrderedBroadcast(i, PERM_PRIVATE, null, null,
+                Activity.RESULT_OK, null, null);
     }
 
     private boolean isNetworkAvailableAndConnected() {
