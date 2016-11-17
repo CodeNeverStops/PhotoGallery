fc8658310fb2136b6c7f39b0a772bebaafb5e40e
Wayne.You <lokiwuxi@gmail.com>
2016-11-17 15:29:06 +0800

    webview can go back

--- app/src/main/java/io/github/uv_lab/photogallery/PhotoPageActivity.java
+++ app/src/main/java/io/github/uv_lab/photogallery/PhotoPageActivity.java
@@ -4,12 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v4.app.Fragment;
+import android.webkit.WebView;
 
 /**
  * Created by youwei on 2016/11/16.
  */
 
 public class PhotoPageActivity extends SingleFragmentActivity {
+
+    private PhotoPageFragment mFragment;
 
     public static Intent newIntent(Context context, Uri photoPageUri) {
         Intent i = new Intent(context, PhotoPageActivity.class);
@@ -19,6 +22,16 @@ public class PhotoPageActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return PhotoPageFragment.newInstance(getIntent().getData());
+        mFragment = PhotoPageFragment.newInstance(getIntent().getData());
+        return mFragment;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mFragment.getWebView().canGoBack()) {
+            mFragment.getWebView().goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
--- app/src/main/java/io/github/uv_lab/photogallery/PhotoPageFragment.java
+++ app/src/main/java/io/github/uv_lab/photogallery/PhotoPageFragment.java
@@ -74,4 +74,8 @@ public class PhotoPageFragment extends VisibleFragment {
 
         return v;
     }
+
+    public WebView getWebView() {
+        return mWebView;
+    }
 }
